c93e8b364380d774c155d5b5ee41ef052a3a0b17
Dilshod <ganiboyevdilshod2002@gmail.com>
2022-12-19 14:19:43 +0500

    token and  username deleleted

--- TelegramBotTest/src/main/java/com/company/container/ComponentContainer.java
+++ TelegramBotTest/src/main/java/com/company/container/ComponentContainer.java
@@ -3,8 +3,8 @@ package com.company.container;
 import com.company.bot.MyBot;
 
 public interface ComponentContainer {
-    String BOT_TOKEN = "5846121933:AAEpGI-H2nMTscEbQ-Z0qQuyH3vEG5GDcVo";
-    String BOT_USERNAME = "t.me/Dilshod1908_bot";
+    String BOT_TOKEN = "";
+    String BOT_USERNAME = "";
 
     MyBot MY_BOT = new MyBot();
 }
